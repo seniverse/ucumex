6e7c8744e6a9afb9163b4cd8d40d3e0cd9fd8583
zhenyi.shu <zhenyi.shu@seniverse.com>
2020-03-04 16:03:33 +0800

    Bump to 0.0.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ucumex.MixProject do
   def project do
     [
       app: :ucumex,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
